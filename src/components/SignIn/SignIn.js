b999d6a0510e4bd97afd6d4fee768613dccc49d1
VladimirDmytriienko <89876159+VladimirDmytriienko@users.noreply.github.com>
2023-11-26 13:40:44 +0200

    Feat: adaptive sign in and sign in handler

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,47 +1,158 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useFormik } from 'formik';
+import { Input} from 'antd'; 
+import axios from 'axios';
 
-import styles from './signIn.module.scss'
+import {ReactComponent as CloseIcon} from '../../assets/img/icons/icons-signIn/close-signIn-icon.svg';
+import miniLogo from '../../assets/img/icons/icons-signIn/mini-logo-signIn.png';
+import errorIcon from '../../assets/img/icons/icons-signIn/error-input-icon.png';
+import googleIcon from '../../assets/img/icons/icons-signIn/google-button-icon.png';
 
-import closeIcon from '../../assets/img/icons/icons-signIn/close-signIn-icon.svg'
+import styles from './signIn.module.scss';
+
+const API_URL = 'http://127.0.0.1:8000/api/v1/auth/login/';
 
 export default function SignIn() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  const handleSignIn = async (values) => {
+    try {
+      const response = await axios.post(API_URL, values);
+      const authToken = response.data.key;
+      localStorage.setItem('authTokenUHelp', authToken);
+      console.log('Sign-in successful:', response.data);
+      closeModal()
+    } catch (error) {
+      console.error('Error during sign-in:', error.message);
+    }
+  };
+  
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validate: (values) => {
+      const errors = {};
+
+      if (!values.email) {
+        errors.email = 'Required';
+      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+        errors.email = 'Введіть дійсну пошту (email@example.com) ';
+      }
+
+      if (!values.password) {
+        errors.password = 'Required';
+      } else if (values.password.length < 6) {
+        errors.password = 'Пароль повинен містити не менше 6 символів.';
+      }
+
+      return errors;
+    },
+    onSubmit: (values) => {
+      handleSignIn(values);
+    },
+  });
+
   return (
-    <div className={styles.shadow}>
-      <div className={styles.signIn}>
-        <div  className={styles.signIn__icon} ><img src={closeIcon} alt='close button'/></div>
-
-        <div className={styles.signIn__wrapper}>
-          <h3 className={styles.signIn__title}>Sign In to your account</h3>
-          <p className={styles.signIn__description}>Please, enter the login details for your account</p>
-          <a className={styles.signIn__google} href='/'>Sign In with Google</a>
-
-          <p className={styles.signIn__separator}>or</p>
-
-          <form className={`styles.signIn__form  form`}>
-            <div className={styles.signIn__form__component}>
-              <label className={styles.form__label} htmlFor='email'>Email:</label>
-              <input className={styles.form__input} placeholder='your.email@example.com' type="email" id="email" name="email" required />
+    <>
+      <p className={styles.signInButton} onClick={openModal}>Sign IN</p>
+      {isOpen && (
+        <div className={`${styles.shadow} ${styles.fadeIn}`}>
+          <div className={`${styles.signIn} `}>
+            <div className={styles.signIn__icon} onClick={closeModal}>
+              <div className={styles.signIn__icon__helper}></div>
+              <img className={styles.signIn__icon__logo}  src={miniLogo}/>
+              <CloseIcon className={styles.signIn__icon__close}/>
             </div>
 
-            <div className={styles.signIn__form__component}>
-              <label className={styles.form__label} htmlFor='password'>Password:</label>
-              <input className={styles.form__input} type='password' id='password' name='password' required />
+            <div className={styles.signIn__wrapper}>
+              <h3 className={styles.signIn__title}>Увійти в аккаунт</h3>
+              <p className={styles.signIn__description}>
+                Будь ласка, введіть дані для входу до акаунту
+              </p>
+              <a className={styles.signIn__google} href='/'>
+                <img src={googleIcon}/> Продовжити через Google
+              </a>
+
+              <p className={styles.signIn__separator}>або</p>
+
+              <form
+                className={`${styles.signIn__form} ${styles.form}`}
+                onSubmit={formik.handleSubmit}
+              >
+                <div className={styles.signIn__form__component}>
+                  <label className={styles.form__label} >
+                    Електронна пошта
+                  </label>
+                  <Input
+                    className={styles.form__input}
+                    status={formik.touched.email && formik.errors.email ? 'error' : ''}
+                    placeholder='your.email@example.com'
+                    type='email'
+                    id='email'
+                    name='email'
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.email}
+                    required
+                  />
+
+                  {formik.touched.email && formik.errors.email && (
+                    <div className={styles.form__error}> <img src={errorIcon} /> {formik.errors.email}</div>
+                  )}
+                </div>
+
+                <div className={styles.signIn__form__component}>
+                  <label className={styles.form__label} >
+                    Пароль:
+                  </label>
+                  <Input.Password
+                    className={styles.form__input}
+                    status={formik.touched.password && formik.errors.password ? 'error' : ''}
+                    id='password'
+                    name='password'
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.password}
+                    required
+                  />
+
+                  {formik.touched.password && formik.errors.password && (
+                    <div className={styles.form__error}> <img src={errorIcon} />{formik.errors.password}</div>
+                  )}
+                </div>
+
+                <a href='/' className={styles.form__forgotPassword}>
+                  Я не пам’ятаю пароль
+                </a>
+                <button  
+                  type='submit' 
+                  className={`${styles.form__submit} ${formik.isValid && formik.touched.email && formik.touched.password ? styles.validSubmit : ''}`}
+                >
+                  Увійти
+                </button>
+              </form>
+
+              <div className={styles.noAccount}>
+                <p className={styles.noAccount__text}>Немає аккаунту?</p>
+                <a href='/' className={styles.noAccount__signUp}>
+                  Зареєструватися
+                </a>
+              </div>
             </div>
-            
-              <a href='/' className={styles.form__forgotPassword}>I forgot my password</a>
-              <a className={styles.form__submit}>Sign In</a>
-
-          </form>
-
-          <div className={styles.noAccount}>
-              <p className={styles.noAccount__text}>Don’t have an account?</p>
-              <a href='/' className={styles.noAccount__signUp}>Sign Up</a>
           </div>
         </div>
-
-
-      </div>     
-    </div>
-
-  )
+      )}
+    </>
+  );
 }
