44399d618305517d637abeee238794d5278488eb
VladimirDmytriienko <89876159+VladimirDmytriienko@users.noreply.github.com>
2023-11-29 15:32:41 +0200

    Feat: again adaptive sign in and sign in handler

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -15,6 +15,7 @@ const API_URL = 'http://127.0.0.1:8000/api/v1/auth/login/';
 export default function SignIn() {
 
   const [isOpen, setIsOpen] = useState(false);
+  const [loginError, setLoginError] = useState(null);
 
   const openModal = () => {
     setIsOpen(true);
@@ -30,11 +31,14 @@ export default function SignIn() {
       const authToken = response.data.key;
       localStorage.setItem('authTokenUHelp', authToken);
       console.log('Sign-in successful:', response.data);
-      closeModal()
+      closeModal();
+      setLoginError(null); 
     } catch (error) {
       console.error('Error during sign-in:', error.message);
+      setLoginError('Пошта або пароль введені не вірно. Спробуйте ще раз');
     }
   };
+  
   
   const formik = useFormik({
     initialValues: {
@@ -71,7 +75,7 @@ export default function SignIn() {
           <div className={`${styles.signIn} `}>
             <div className={styles.signIn__icon} onClick={closeModal}>
               <div className={styles.signIn__icon__helper}></div>
-              <img className={styles.signIn__icon__logo}  src={miniLogo}/>
+              <img className={styles.signIn__icon__logo}  src={miniLogo} alt='UHelp logo'/>
               <CloseIcon className={styles.signIn__icon__close}/>
             </div>
 
@@ -81,7 +85,7 @@ export default function SignIn() {
                 Будь ласка, введіть дані для входу до акаунту
               </p>
               <a className={styles.signIn__google} href='/'>
-                <img src={googleIcon}/> Продовжити через Google
+                <img src={googleIcon} alt='google logo'/> Продовжити через Google
               </a>
 
               <p className={styles.signIn__separator}>або</p>
@@ -92,11 +96,11 @@ export default function SignIn() {
               >
                 <div className={styles.signIn__form__component}>
                   <label className={styles.form__label} >
-                    Електронна пошта
+                    Пошта
                   </label>
                   <Input
                     className={styles.form__input}
-                    status={formik.touched.email && formik.errors.email ? 'error' : ''}
+                    status={formik.touched.email && formik.errors.email || loginError ? 'error' : ''}
                     placeholder='your.email@example.com'
                     type='email'
                     id='email'
@@ -104,11 +108,12 @@ export default function SignIn() {
                     onChange={formik.handleChange}
                     onBlur={formik.handleBlur}
                     value={formik.values.email}
+                    autoComplete='email'
                     required
                   />
 
                   {formik.touched.email && formik.errors.email && (
-                    <div className={styles.form__error}> <img src={errorIcon} /> {formik.errors.email}</div>
+                    <div className={styles.form__error}> <img src={errorIcon} alt='Error icon' /> {formik.errors.email}</div>
                   )}
                 </div>
 
@@ -118,18 +123,26 @@ export default function SignIn() {
                   </label>
                   <Input.Password
                     className={styles.form__input}
-                    status={formik.touched.password && formik.errors.password ? 'error' : ''}
+                    status={formik.touched.password && formik.errors.password || loginError ? 'error' : ''}
                     id='password'
                     name='password'
                     onChange={formik.handleChange}
                     onBlur={formik.handleBlur}
                     value={formik.values.password}
+                    autoComplete='on'
                     required
                   />
 
                   {formik.touched.password && formik.errors.password && (
-                    <div className={styles.form__error}> <img src={errorIcon} />{formik.errors.password}</div>
+                    <div className={styles.form__error}> <img src={errorIcon} alt='Error icon' />{formik.errors.password}</div>
                   )}
+                  {loginError && (
+                      <div className={styles.form__error}>
+                        <img src={errorIcon} alt='Error icon' />
+                        {loginError}
+                      </div>
+                    )}
+
                 </div>
 
                 <a href='/' className={styles.form__forgotPassword}>
