125e485cb166b5e864f856a0c9eac8cf257ba49e
Eugene <evgenkuzmenko94@gmail.com>
2024-02-01 22:17:24 +0200

    added icon

--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -28,8 +28,12 @@ export default function UserPage() {
 
 				const userResponse = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/v1/auth/user/`, config);
 
+				console.log(userResponse)
+
 				const expertUserProfileData = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/v1/users/expert_user_profile_by_user_id/${userResponse.data.pk}/`,
 					config);
+
+				console.log(expertUserProfileData)
 
 
 				setUserDataPortfolio(expertUserProfileData.data.portfolio)
