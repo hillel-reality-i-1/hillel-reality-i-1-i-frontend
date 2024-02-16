8fb106d1b2e28ea5ba3aabd45aaa293635ce3ea1
VladimirDmytriienko <89876159+VladimirDmytriienko@users.noreply.github.com>
2024-02-16 10:59:25 +0200

    fix: add some chanches

--- src/components/SettingsComponents/PersonalInfo/PersonalInfo.js
+++ src/components/SettingsComponents/PersonalInfo/PersonalInfo.js
@@ -2,11 +2,14 @@ import { Link } from 'react-router-dom';
 
 import { ReactComponent as UserSettings } from '../../../assets/img/icons/settings-icons/user-settings-grey.svg';
 import { ReactComponent as Arrow } from '../../../assets/img/icons/settings-icons/arrow.svg';
-
+import { ReactComponent as FacebookIcon } from '../../../assets/img/icons/settings-icons/socials-icons/Socials-facebook.svg';
+import { ReactComponent as InstIcon } from '../../../assets/img/icons/settings-icons/socials-icons/Socials-inst.svg';
+import { ReactComponent as LinkedinIcon } from '../../../assets/img/icons/settings-icons/socials-icons/Socials-linkedin.svg';
+import { ReactComponent as TelegramIcon } from '../../../assets/img/icons/settings-icons/socials-icons/Socials-telegram.svg';
 import styles from './personalInfo.module.scss';
 
 export default function PersonalInfo({ data }) {
-
+  console.log(data);
   return (
     <>
       <h2 className={styles.title}><UserSettings /> Особиста інформація </h2>
@@ -25,7 +28,15 @@ export default function PersonalInfo({ data }) {
           ? `${data.city.name},  ${data.country.name}`
           : ''} <Arrow /> </Link> </div>
         <div className={styles.info}><p className={styles.info__type}>Інфо</p> <Link className={styles.info__link} to='biography'><span>{data.about_my_self}</span><Arrow /> </Link> </div>
-        <div className={styles.info}><p className={styles.info__type}>Соціальні мережі</p> <Link className={styles.info__link} to='socials'> <Arrow /> </Link> </div>
+        <div className={styles.info}><p className={styles.info__type}>Соціальні мережі</p> <Link className={styles.info__link} to='socials'>
+          <div style={{ display: 'flex', gap: '6px' }}>
+            {data?.instagram ? <InstIcon /> : ''}
+            {data?.telegram ? <TelegramIcon /> : ''}
+            {data?.facebook ? <FacebookIcon /> : ''}
+            {data?.linkedin ? <LinkedinIcon /> : ''}
+          </div>
+
+          <Arrow /> </Link> </div>
       </div>
     </>
   )
